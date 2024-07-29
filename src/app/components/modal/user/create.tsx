11dc85056358a6f7e13be975bb701a3694a89f50
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-07-29 17:32:45 +0100

    feat: Hook Resolver

--- src/app/components/modal/user/create.tsx
+++ src/app/components/modal/user/create.tsx
@@ -4,6 +4,8 @@ import { UserService } from "../../../../services/user";
 import {toast} from "react-hot-toast";
 import { UserDTO } from "../../../../utils/types";
 import { useForm } from "react-hook-form";
+import {zodResolver} from '@hookform/resolvers/zod';
+import { UserSchema } from "../../../../utils/schemas";
 
 type Props = {
     close: () => void;
@@ -12,7 +14,9 @@ type Props = {
 export function Create({close}:Props) {
     const userService = new UserService();
     const queryClient = useQueryClient();
-    const {register,handleSubmit} = useForm<UserDTO>();
+    const {register,handleSubmit,formState: {errors}} = useForm<UserDTO>({
+        resolver: zodResolver(UserSchema) 
+    });
 
     const mutation = useMutation({
         mutationFn: async (data: UserDTO) => await userService.create(data),
@@ -42,14 +46,17 @@ export function Create({close}:Props) {
                     <div className="w-full">
                         <label htmlFor="name" className="text-sm mb-1 text-slate-700">Name</label>
                         <input {...register('firstName')} type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
+                        {errors?.firstName && <span className="text-xs text-red-500">{errors?.firstName.message}</span>}
                     </div>
                     <div className="w-full">
                         <label htmlFor="lastName" className="text-sm mb-1 text-slate-700">LastName</label>
                         <input {...register('lastName')} type="text" id="lastName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
+                        {errors?.lastName && <span className="text-xs text-red-500">{errors?.lastName.message}</span>}
                     </div>
                     <div className="w-full">
                         <label htmlFor="email" className="text-sm mb-1 text-slate-700">E-mail</label>
                         <input {...register('email')} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
+                        {errors?.email && <span className="text-xs text-red-500">{errors?.email.message}</span>}
                     </div>
                     <button className="inline-flex gap-2 items-center justify-center rounded-xl bg-gradient-to-r from-orange-600 to-orange-500 py-3 px-5 text-sm font-medium text-white shadow-xl shadow-gray-400/75 transition-transform duration-200 hover:scale-[1.01]">
                     Submit
